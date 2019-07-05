ea5f0d934ca6e3460e7d86ad878c508ddd1d75f0
Philippe Miossec <pmiossec@gmail.com>
2019-07-05 13:22:45 +0200

    Improve User input prompt

---
+++ Plugins/GitHub3/UserInputPrompt.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GitHub3
+{
+    partial class UserInputPrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.txt_UserInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(194, 47);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.btn_OK.TabIndex = 0;
+            this.btn_OK.Text = "&OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // txt_UserInput
+            // 
+            this.txt_UserInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_UserInput.Location = new System.Drawing.Point(12, 21);
+            this.txt_UserInput.Name = "txt_UserInput";
+            this.txt_UserInput.Size = new System.Drawing.Size(257, 20);
+            this.txt_UserInput.TabIndex = 1;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(10, 5);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(127, 13);
+            this.labelInput.TabIndex = 2;
+            this.labelInput.Text = "Please specify your input:";
+            // 
+            // UserInputPrompt
+            // 
+            this.AcceptButton = this.btn_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(281, 73);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.txt_UserInput);
+            this.Controls.Add(this.btn_OK);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(297, 112);
+            this.Name = "UserInputPrompt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "UserInputPrompt";
+            this.Shown += new System.EventHandler(this.SimplePrompt_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.TextBox txt_UserInput;
+        private System.Windows.Forms.Label labelInput;
+    }
+}
---
+++ Plugins/GitHub3/UserInputPrompt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GitHub3
+{
+    public partial class UserInputPrompt : Form
+    {
+        public string UserInput { get; private set; } = "";
+
+        public UserInputPrompt(string title, string label, bool isPassword = false)
+        {
+            InitializeComponent();
+            Text = title;
+            labelInput.Text = label;
+            if (isPassword)
+            {
+                txt_UserInput.PasswordChar = '*';
+            }
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            UserInput = txt_UserInput.Text;
+            Close();
+        }
+
+        private void SimplePrompt_Shown(object sender, EventArgs e)
+        {
+            txt_UserInput.Focus();
+        }
+    }
+}
