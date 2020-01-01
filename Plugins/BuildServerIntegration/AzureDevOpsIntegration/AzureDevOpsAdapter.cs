2a639202518c372aa935a5ee3808ada64de8ae3b
Philippe Miossec <pmiossec@gmail.com>
2020-01-01 23:36:55 +0100

    Cache finished builds results (to prevent a costly call at every grid revision refresh)

--- Plugins/BuildServerIntegration/AzureDevOpsIntegration/AzureDevOpsAdapter.cs
+++ Plugins/BuildServerIntegration/AzureDevOpsIntegration/AzureDevOpsAdapter.cs
@@ -139,13 +139,28 @@ namespace AzureDevOpsIntegration
                     return;
                 }
 
+                if (!running && !sinceDate.HasValue && CacheAzureDevOps.FinishedBuilds.Any())
+                {
+                    foreach (var buildInfo in CacheAzureDevOps.FinishedBuilds)
+                    {
+                        observer.OnNext(buildInfo);
+                    }
+
+                    return;
+                }
+
                 var builds = running ?
                     FilterRunningBuilds(await _apiClient.QueryRunningBuildsAsync(_buildDefinitions)) :
                     await _apiClient.QueryFinishedBuildsAsync(_buildDefinitions, sinceDate);
 
                 foreach (var build in builds)
                 {
-                    observer.OnNext(CreateBuildInfo(build));
+                    var buildInfo = CreateBuildInfo(build);
+                    observer.OnNext(buildInfo);
+                    if (!running)
+                    {
+                        CacheAzureDevOps.FinishedBuilds.Add(buildInfo);
+                    }
                 }
             }
             catch (OperationCanceledException)
