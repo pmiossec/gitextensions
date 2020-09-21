b9b0fb123c3a2b12bf29aca62a568a94675db40c
Philippe Miossec <pmiossec@gmail.com>
2020-09-21 12:10:58 +0200

    Add icon for currently checked out branch

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -1936,6 +1936,7 @@ namespace GitUI.CommandsDialogs
         private void CurrentBranchDropDownOpening(object sender, EventArgs e)
         {
             branchSelect.DropDown.SuspendLayout();
+            var selectedBranch = branchSelect.Text;
             branchSelect.DropDownItems.Clear();
 
             AddCheckoutBranchMenuItem();
@@ -1966,7 +1967,12 @@ namespace GitUI.CommandsDialogs
 
                     ToolStripItem toolStripItem = branchSelect.DropDownItems.Add(branch.Name);
                     toolStripItem.ForeColor = isBranchVisible ? branchSelect.ForeColor : Color.Silver.AdaptTextColor();
-                    toolStripItem.Image = isBranchVisible ? Images.Branch : Images.EyeClosed;
+                    toolStripItem.Image = isBranchVisible ? null : Images.EyeClosed;
+
+                    if (branch.Name == selectedBranch)
+                    {
+                        toolStripItem.Image = Resources.branch;
+                    }
 
                     if (branch.Name == "master")
                     {
