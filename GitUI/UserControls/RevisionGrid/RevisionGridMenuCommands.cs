69cc9545c487a75d3e368ded2bb708ca3b3fbc37
Philippe Miossec <pmiossec@gmail.com>
2022-09-13 12:07:55 +0200

    FormBrows: Add "Find in file tree" in the menu.

--- GitUI/UserControls/RevisionGrid/RevisionGridMenuCommands.cs
+++ GitUI/UserControls/RevisionGrid/RevisionGridMenuCommands.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using GitCommands;
+using GitUI.CommandsDialogs;
 using GitUI.CommandsDialogs.BrowseDialog;
 using GitUI.Hotkey;
 using GitUI.Properties;
@@ -177,6 +178,14 @@ namespace GitUI.UserControls.RevisionGrid
                     Text = "Quick search &next",
                     ShortcutKeyDisplayString = GetShortcutKeyDisplayStringFromRevisionGridIfAvailable(RevisionGridControl.Command.NextQuickSearch),
                     ExecuteAction = () => _revisionGrid.ExecuteCommand(RevisionGridControl.Command.NextQuickSearch)
+                },
+                MenuCommand.CreateSeparator(),
+                new MenuCommand
+                {
+                    Name = "FindInFileTree",
+                    Text = "Find in file tree",
+                    ShortcutKeyDisplayString = (Keys.Control | Keys.Shift | Keys.F).ToText(),
+                    ExecuteAction = () => SendKeys.Send("^+F")
                 }
             };
         }
