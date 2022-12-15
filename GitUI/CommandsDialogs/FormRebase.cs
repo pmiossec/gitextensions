16fb4ab3807806342ecebcaeea129cc567880ea1
Philippe Miossec <pmiossec@gmail.com>
2022-12-15 11:21:16 +0100

    Rebase: hide stashes in revision grid when selecting commit range

--- GitUI/CommandsDialogs/FormRebase.cs
+++ GitUI/CommandsDialogs/FormRebase.cs
@@ -371,9 +371,11 @@ namespace GitUI.CommandsDialogs
             bool previousValueBranchFilterEnabled = AppSettings.BranchFilterEnabled;
             bool previousValueShowCurrentBranchOnly = AppSettings.ShowCurrentBranchOnly;
             bool previousValueShowReflogReferences = AppSettings.ShowReflogReferences;
+            bool previousValueShowStashes = AppSettings.ShowStashes;
 
             try
             {
+                AppSettings.ShowStashes = false;
                 using FormChooseCommit chooseForm = new(UICommands, txtFrom.Text, showCurrentBranchOnly: true);
                 if (chooseForm.ShowDialog(this) == DialogResult.OK && chooseForm.SelectedRevision is not null)
                 {
@@ -382,6 +384,7 @@ namespace GitUI.CommandsDialogs
             }
             finally
             {
+                AppSettings.ShowStashes = previousValueShowStashes;
                 AppSettings.BranchFilterEnabled = previousValueBranchFilterEnabled;
                 AppSettings.ShowCurrentBranchOnly = previousValueShowCurrentBranchOnly;
                 AppSettings.ShowReflogReferences = previousValueShowReflogReferences;
