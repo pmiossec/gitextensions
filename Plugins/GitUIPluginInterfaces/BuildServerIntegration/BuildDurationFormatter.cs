4e52ffa5bcae9c4f6aea93471090512b7b9d886b
Philippe Miossec <pmiossec@gmail.com>
2023-04-18 12:18:15 +0200

    AppVeyor: better build description
    Remove skipped display because that's not really useful
    and that's just result on the current test run and not the total of all skipped
    (so the display is strange and could decrease....)

--- Plugins/GitUIPluginInterfaces/BuildServerIntegration/BuildDurationFormatter.cs
+++ Plugins/GitUIPluginInterfaces/BuildServerIntegration/BuildDurationFormatter.cs
@@ -2,17 +2,17 @@
 {
     public interface IBuildDurationFormatter
     {
-        string Format(long? durationMilliseconds);
+        string Format(long? durationMilliseconds, bool wrapWithParenthesis = true);
     }
 
     public class BuildDurationFormatter : IBuildDurationFormatter
     {
-        public string Format(long? durationMilliseconds)
+        public string Format(long? durationMilliseconds, bool wrapWithParenthesis)
         {
             if (durationMilliseconds.HasValue)
             {
                 string timeText = TimeSpan.FromMilliseconds(durationMilliseconds.Value).ToString(@"mm\:ss");
-                return $"({timeText})";
+                return wrapWithParenthesis ? $"({timeText})" : timeText;
             }
 
             return string.Empty;
