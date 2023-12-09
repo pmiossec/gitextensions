6b1161cddf53c7381d46cd77bd87baaee51a1258
Philippe Miossec <pmiossec@gmail.com>
2023-12-09 10:52:18 +0100

    RevisionGrid: being able to filter to see only the last n days

--- src/app/GitUI/UserControls/RevisionGrid/FilterInfo.cs
+++ src/app/GitUI/UserControls/RevisionGrid/FilterInfo.cs
@@ -9,6 +9,7 @@ public record FilterInfo
 {
     private DateTime _dateFrom;
     private DateTime _dateTo;
+    private uint? _lastDaysCount = null;
     private string _author = string.Empty;
     private string _committer = string.Empty;
     private string _message = string.Empty;
@@ -37,6 +38,12 @@ public record FilterInfo
     {
         get => GetValue(ByDateTo, _dateTo, DateTime.MinValue);
         set => _dateTo = value;
+    }
+
+    public uint? LastDaysCount
+    {
+        get => _lastDaysCount;
+        set => _lastDaysCount = value;
     }
 
     public bool ByAuthor { get; set; }
@@ -334,6 +341,12 @@ public record FilterInfo
         if (CommitsLimit > 0)
         {
             filter.Add($"--max-count={CommitsLimit}");
+        }
+
+        if (LastDaysCount.HasValue)
+        {
+            DateTime date = DateTime.Now.AddDays(-1 * LastDaysCount.Value);
+            filter.Add($"--since=\"{date:yyyy-MM-dd hh:mm:ss}\"");
         }
 
         if (ByDateFrom)
