3bfb10189435845eaa68a924f28c5af0e1ed8af8
Philippe Miossec <pmiossec@gmail.com>
2023-12-11 12:42:20 +0100

    FormDeleteBranch: check that the HEAD of the deleted branch is in the reflog
    * Add immediate feedback to the user (is or is not in reflog)
    * Improve the hint when not recoverable by reflog
    * Override user confirmation setting **if commit is not in the reflog**
       to prevent user to **really** loose commits

--- src/app/GitUI/CommandsDialogs/FormDeleteBranch.cs
+++ src/app/GitUI/CommandsDialogs/FormDeleteBranch.cs
@@ -13,11 +13,16 @@ namespace GitUI.CommandsDialogs
         private readonly TranslationString _cannotDeleteCurrentBranchMessage = new("Cannot delete the branch “{0}” which you are currently on.");
         private readonly TranslationString _deleteBranchConfirmTitle = new("Delete Confirmation");
         private readonly TranslationString _deleteBranchQuestion = new("The selected branch(es) have not been merged into HEAD.\r\nProceed?");
-        private readonly TranslationString _useReflogHint = new("Did you know you can use reflog to restore deleted branches?");
+        private readonly TranslationString _useReflogHint = new("The commits are available from the reflog that you could use to restore the deleted branch(es)");
+        private readonly TranslationString _deleteBranchNotInReflogQuestion = new("At least one of the selected branch(es) have not been merged into HEAD\r\n and is not in the reflog.\r\n\r\nSo you won't be able to recover the commit(s) easily.\r\n\r\nProceed?");
+        private readonly TranslationString _useRecoverLostObjectsHint = new("This commit will only be recoverable using \"Recover lost objects\" feature but be aware that git could remove it at any time!");
+        private readonly TranslationString _restoreUsingReflogAvailable = new("This branch can be restored using the reflog");
+        private readonly TranslationString _warningNotInReflog = new("Warning! The head of this branch is not in the reflog!\r\nCommits won't be recoverable easily!!");
 
         private readonly IEnumerable<string> _defaultBranches;
         private string? _currentBranch;
         private HashSet<string>? _mergedBranches;
+        private IReadOnlyList<string> _reflogHashes;
 
         public FormDeleteBranch(IGitUICommands commands, IEnumerable<string> defaultBranches)
             : base(commands, enablePositionRestore: false)
@@ -34,6 +39,8 @@ namespace GitUI.CommandsDialogs
         protected override void OnRuntimeLoad(EventArgs e)
         {
             base.OnRuntimeLoad(e);
+
+            _reflogHashes = Module.GetReflogHashes();
 
             Branches.BranchesToSelect = Module.GetRefs(RefsFilter.Heads).ToList();
             if (AppSettings.DontConfirmDeleteUnmergedBranch)
@@ -63,6 +70,8 @@ namespace GitUI.CommandsDialogs
             }
 
             Branches.Focus();
+
+            CheckSelectedBranches();
         }
 
         private void Delete_Click(object sender, EventArgs e)
@@ -79,7 +88,9 @@ namespace GitUI.CommandsDialogs
                 return;
             }
 
-            if (!AppSettings.DontConfirmDeleteUnmergedBranch)
+            bool areAllInReflog = selectedBranches.All(b => _reflogHashes.Contains(b.ObjectId.ToString()));
+
+            if (!areAllInReflog || !AppSettings.DontConfirmDeleteUnmergedBranch)
             {
                 Validates.NotNull(_mergedBranches);
 
@@ -90,12 +101,12 @@ namespace GitUI.CommandsDialogs
                 {
                     TaskDialogPage page = new()
                     {
-                        Text = _deleteBranchQuestion.Text,
+                        Text = areAllInReflog ? _deleteBranchQuestion.Text : _deleteBranchNotInReflogQuestion.Text,
                         Caption = _deleteBranchConfirmTitle.Text,
                         Icon = TaskDialogIcon.Warning,
                         Buttons = { TaskDialogButton.Yes, TaskDialogButton.No },
                         DefaultButton = TaskDialogButton.No,
-                        Footnote = _useReflogHint.Text,
+                        Footnote = areAllInReflog ? _useReflogHint.Text : _useRecoverLostObjectsHint.Text,
                         SizeToContent = true,
                     };
 
@@ -113,6 +124,33 @@ namespace GitUI.CommandsDialogs
             {
                 Close();
             }
+        }
+
+        private void Branches_SelectedValueChanged(object sender, EventArgs e)
+        {
+            CheckSelectedBranches();
+        }
+
+        private void CheckSelectedBranches()
+        {
+            IGitRef[] selectedBranches = Branches.GetSelectedBranches().ToArray();
+            if (!selectedBranches.Any())
+            {
+                return;
+            }
+
+            foreach (IGitRef selectedBranch in selectedBranches)
+            {
+                if (!_reflogHashes.Contains(selectedBranch.ObjectId.ToString()))
+                {
+                    labelWarning.Text = _warningNotInReflog.Text;
+                    labelWarning.ForeColor = Color.Orange;
+                    return;
+                }
+            }
+
+            labelWarning.Text = _restoreUsingReflogAvailable.Text;
+            labelWarning.ForeColor = Color.Green;
         }
     }
 }
