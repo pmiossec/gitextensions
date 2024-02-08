b29e80c05f65693637d09ab5bd973c7c81702ec5
Philippe Miossec <pmiossec@gmail.com>
2024-02-08 10:28:26 +0100

    FilterToolBar: Increase size of filter boxes when entering field

--- src/app/GitUI/UserControls/FilterToolBar.Designer.cs
+++ src/app/GitUI/UserControls/FilterToolBar.Designer.cs
@@ -179,6 +179,8 @@ namespace GitUI.UserControls
             tscboBranchFilter.KeyUp += tscboBranchFilter_KeyUp;
             tscboBranchFilter.TextChanged += tscboBranchFilter_TextChanged;
             tscboBranchFilter.TextUpdate += tscboBranchFilter_TextUpdate;
+            tscboBranchFilter.Enter += TscboBranchFilter_Enter;
+            tscboBranchFilter.Leave += TscboBranchFilter_Leave;
             // 
             // tsmiBranchLocal
             // 
@@ -235,6 +237,8 @@ namespace GitUI.UserControls
             tstxtRevisionFilter.Size = new Size(100, 25);
             tstxtRevisionFilter.Tag = "ToolBar_group:Text filter";
             tstxtRevisionFilter.KeyUp += tstxtRevisionFilter_KeyUp;
+            tstxtRevisionFilter.Enter += TstxtRevisionFilter_Enter;
+            tstxtRevisionFilter.Leave += TstxtRevisionFilter_Leave;
             // 
             // tsddbtnRevisionFilter
             // 
