ddec47dc669a7a8d934920e7a1c7cf399d55850b
Philippe Miossec <pmiossec@gmail.com>
2024-07-11 11:04:27 +0200

    LeftPanel: found node color customizable

--- src/app/GitUI/LeftPanel/RepoObjectsTree.cs
+++ src/app/GitUI/LeftPanel/RepoObjectsTree.cs
@@ -374,8 +374,10 @@ public sealed partial class RepoObjectsTree : GitModuleControl
 
     private static void AddTreeNodeToSearchResult(ICollection<TreeNode> ret, TreeNode node)
     {
-        node.BackColor = SystemColors.Info;
+        node.BackColor = AppColor.HighlightAllOccurences.GetThemeColor();
         node.ForeColor = SystemColors.InfoText;
+        ////node.BackColor = AppColor.AuthoredHighlight.GetThemeColor();
+        ////node.ForeColor = node.BackColor.GetContrastColor();
         ret.Add(node);
     }
 
