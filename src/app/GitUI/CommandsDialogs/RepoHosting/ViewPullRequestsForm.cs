7549ab6999c6a3eb4cceb2a4900fe3da773db6c8
Philippe Miossec <pmiossec@gmail.com>
2024-09-09 13:58:21 +0200

    perfs: fill ComboBoxes with AddRange instead of Add
    See "Control population" in https://learn.microsoft.com/en-us/archive/msdn-magazine/2006/march/practical-tips-for-boosting-the-performance-of-windows-forms-apps

--- src/app/GitUI/CommandsDialogs/RepoHosting/ViewPullRequestsForm.cs
+++ src/app/GitUI/CommandsDialogs/RepoHosting/ViewPullRequestsForm.cs
@@ -252,21 +252,18 @@ namespace GitUI.CommandsDialogs.RepoHosting
         private void LoadListView()
         {
             Validates.NotNull(_pullRequestsInfo);
-            foreach (IPullRequestInformation info in _pullRequestsInfo)
+            _pullRequestsList.Items.AddRange(_pullRequestsInfo.Select(info => new ListViewItem
             {
-                _pullRequestsList.Items.Add(new ListViewItem
-                {
-                    Text = info.Id,
-                    Tag = info,
-                    SubItems =
+                Text = info.Id,
+                Tag = info,
+                SubItems =
                     {
                         info.Title,
                         info.Owner,
                         info.Created.ToString(),
                         info.FetchBranch,
                     }
-                });
-            }
+            }).ToArray());
 
             ResizeColumnsToFitContent();
 
