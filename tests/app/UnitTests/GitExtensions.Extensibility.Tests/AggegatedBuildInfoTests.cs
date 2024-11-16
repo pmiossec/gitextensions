913e7f6e54618c71a3d4d7fdd504a150514b0ec6
Philippe Miossec <pmiossec@gmail.com>
2024-11-16 22:18:34 +0100

    fix aggregation (a BuildInfo override an AggegatedBuildInfo :( )

--- tests/app/UnitTests/GitExtensions.Extensibility.Tests/AggegatedBuildInfoTests.cs
+++ tests/app/UnitTests/GitExtensions.Extensibility.Tests/AggegatedBuildInfoTests.cs
@@ -12,10 +12,9 @@ public sealed class AggegatedBuildInfoTests
     public void Should_select_only_build_as_default_one()
     {
         IBuildInfo defaultBuild = SuccessBuild(1);
-        AggegatedBuildInfo sut = new()
-        {
-            Builds = [defaultBuild]
-        };
+        AggegatedBuildInfo sut = new();
+
+        sut.AddBuilds(new[] { defaultBuild });
 
         sut.DefaultBuild.Should().Be(defaultBuild);
     }
@@ -24,10 +23,9 @@ public sealed class AggegatedBuildInfoTests
     public void Should_select_the_more_recent_build_result()
     {
         IBuildInfo moreRecentBuild = SuccessBuild(12);
-        AggegatedBuildInfo sut = new()
-        {
-            Builds = [SuccessBuild(10), moreRecentBuild]
-        };
+        AggegatedBuildInfo sut = new();
+
+        sut.AddBuilds([SuccessBuild(10), moreRecentBuild]);
 
         sut.DefaultBuild.Should().Be(moreRecentBuild);
     }
@@ -36,10 +34,9 @@ public sealed class AggegatedBuildInfoTests
     public void Should_select_the_last_succeeded_build_result_of_job_when_muliple_jobs()
     {
         IBuildInfo moreRecentBuild = SuccessBuild(12, "1");
-        AggegatedBuildInfo sut = new()
-        {
-            Builds = [SuccessBuild(10, "2"), SuccessBuild(10, "1"), moreRecentBuild]
-        };
+        AggegatedBuildInfo sut = new();
+
+        sut.AddBuilds([SuccessBuild(10, "2"), SuccessBuild(10, "1"), moreRecentBuild]);
 
         sut.DefaultBuild.Should().Be(moreRecentBuild);
     }
@@ -48,10 +45,9 @@ public sealed class AggegatedBuildInfoTests
     public void Should_select_the_broken_build_result_of_job_when_muliple_jobs_and_broken_is_the_more_recent_of_given_job()
     {
         IBuildInfo moreRecentBrokenBuild = new BuildInfo() { Status = BuildStatus.Failure, StartDate = Date(12), BuildDefinitionName = "1" };
-        AggegatedBuildInfo sut = new()
-        {
-            Builds = [SuccessBuild(14, "2"), SuccessBuild(10, "1"), moreRecentBrokenBuild]
-        };
+        AggegatedBuildInfo sut = new();
+
+        sut.AddBuilds([SuccessBuild(14, "2"), SuccessBuild(10, "1"), moreRecentBrokenBuild]);
 
         sut.DefaultBuild.Should().Be(moreRecentBrokenBuild);
     }
