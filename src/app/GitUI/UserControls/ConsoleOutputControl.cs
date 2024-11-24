0d024a8cf58358c7b4cb38c2b11432aa72c4fd99
Philippe Miossec <pmiossec@gmail.com>
2024-11-24 15:46:12 +0100

    refactor: convert to .net6 argument null check syntax

--- src/app/GitUI/UserControls/ConsoleOutputControl.cs
+++ src/app/GitUI/UserControls/ConsoleOutputControl.cs
@@ -41,10 +41,7 @@ namespace GitUI.UserControls
 
         protected void FireDataReceived(TextEventArgs args)
         {
-            if (args is null)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
+            ArgumentNullException.ThrowIfNull(args);
 
             DataReceived?.Invoke(this, args);
         }
